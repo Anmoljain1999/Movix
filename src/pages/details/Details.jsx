c0c44e02856ad51bb90cc295d361a2e366557cec
anmol <202011010@diu.iiitvadodara.ac.in>
2023-12-11 10:40:28 +0530

    changes

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./style.scss"
-import useFetch from '../../hooks/useFetch';
+import useFetch from '../../hooks/UseFetch';
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import Cast from './cast/Cast';
